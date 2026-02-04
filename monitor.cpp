8c07fe5e3036c50c504b8963ef2a06075f3c2b52
Venugopal K <venu.shastri@gmail.com>
2026-02-04 16:03:02 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -5,6 +5,54 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
+//Format functionality
+void blinkAlert() {
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+}
+/* Decision Logic Functions */
+bool isTemperatureCritical(float temperature) {
+  return temperature > 102 || temperature < 95;
+}
+
+bool isPulseRateCritical(float pulseRate) {
+  return pulseRate < 60 || pulseRate > 100;
+}
+
+bool isSpo2Critical(float spo2) {
+  return spo2 < 90;
+}
+
+//Single Responsibility Principle Applied
+//Co-oridanator- Facade Api
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+
+  if (isTemperatureCritical(temperature)) {
+    cout << "Temperature is critical!\n";
+    blinkAlert();
+    return 0;
+  }
+
+  if (isPulseRateCritical(pulseRate)) {
+    cout << "Pulse Rate is out of range!\n";
+    blinkAlert();
+    return 0;
+  }
+
+  if (isSpo2Critical(spo2)) {
+    cout << "Oxygen Saturation out of range!\n";
+    blinkAlert();
+    return 0;
+  }
+
+  return 1;
+}
+
+/*
 int vitalsOk(float temperature, float pulseRate, float spo2) {
   if (temperature > 102 || temperature < 95) {
     cout << "Temperature is critical!\n";
@@ -35,4 +83,4 @@ int vitalsOk(float temperature, float pulseRate, float spo2) {
     return 0;
   }
   return 1;
-}
+}*/
