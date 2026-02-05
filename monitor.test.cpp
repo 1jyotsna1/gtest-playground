1052087ee3e5eb64d3954813b11ea2776ca07025
Venugopal K <venu.shastri@gmail.com>
2026-02-05 15:35:24 +0530

    Test Double in use

--- monitor.test.cpp
+++ monitor.test.cpp
@@ -1,9 +1,17 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
+//Fake Dependencies - substitute real dependencies
+void DisplayTestDouble(string content){
+
+}
+
+void BlinkTestDouble(){
+
+}
 
 TEST(MonitorTest,VitalAllOkTest){
     //Happy Path
-int actualResult=vitalsOk(98.6f,72.0f,98.0f);
+int actualResult=vitalsOk(98.6f,72.0f,98.0f,&DisplayTestDouble,&BlinkTestDouble);
 EXPECT_EQ(actualResult,1);
 }
