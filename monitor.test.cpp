28aee6627d9cd27eb1478d53a745cfbc0bb5bf3c
Venugopal K <venu.shastri@gmail.com>
2026-02-05 15:45:25 +0530

    mocking

--- monitor.test.cpp
+++ monitor.test.cpp
@@ -3,17 +3,34 @@
 
 
 
-//Fake Dependencies - substitute real dependencies
+//Fake Dependencies - substitute real dependencies, stub
 void DisplayTestDouble(string content){
 
 }
-
+//stub
 void BlinkTestDouble(){
 
 }
 
+//smart stubs -> Mock
+//Record interaction
+
+//Mock state
+int displayCallCount=0;
+int blickCallCount=0;
+
+void DisplayTestDouble_mock(string content){
+displayCallCount+=1;
+}
+//stub
+void BlinkTestDouble_mock(){
+blickCallCount+=1;
+}
 TEST(MonitorTest,VitalAllOkTest){
     //Happy Path
 int actualResult=vitalsOk(98.6f,72.0f,98.0f,&DisplayTestDouble,&BlinkTestDouble);
+//Asser on Mock State
+EXPECT_EQ(displayCallCount,0);
+EXPECT_EQ(displayCallCount,0);
 EXPECT_EQ(actualResult,1);
 }
